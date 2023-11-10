13f9f055e945a5c1c4130f6179eeb55e4ba0b9f9
ileosebastian <ileosebastian@gmail.com>
2023-11-10 10:38:28 -0500

    Agregado la logica para saber si va a ejecutar la bd en web.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
@@ -6,6 +6,24 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+
+import { Capacitor } from '@capacitor/core';
+import { defineCustomElements as jeepSqlite } from 'jeep-sqlite/loader';
+
+const platform = Capacitor.getPlatform();
+
+if (platform === 'web') {
+  jeepSqlite(window);
+
+  window.addEventListener(
+    'DOMContentLoaded',
+    () => {
+      const jeepElement = document.createElement('jeep-sqlite');
+      document.body.appendChild(jeepElement);
+    }
+  );
+}
+
 
 if (environment.production) {
   enableProdMode();
